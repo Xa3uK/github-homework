d6a9ae9af18b012594582da69c336b1e170f470b
Xa3uK <hazardsales@gmail.com>
2021-11-02 17:01:25 +0200

    Added AnimalFactory

---
+++ lesson12/App.java
@@ -0,0 +1,24 @@
+package lesson12;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        appRun();
+    }
+
+    public static void appRun() {
+        System.out.println("Какое животное создать?");
+        String userAnswer = userAsk();
+        while (AnimalFactory.getAnimalByKey(userAnswer) == null) {
+            System.out.println("Не могу создать Animal. Try again:");
+            userAnswer = userAsk();
+        }
+        System.out.println(AnimalFactory.getAnimalByKey(userAnswer).getName());
+    }
+
+    public static String userAsk() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+}
