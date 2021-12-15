28655dd004ec844f3b7c4cbb916f41972bd45e79
Xa3uK <hazardsales@gmail.com>
2021-12-15 19:01:33 +0200

    SortLetter

---
+++ lesson23/Reader.java
@@ -0,0 +1,24 @@
+package lesson23;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class Reader {
+
+    private final static String fileName = "lesson23/resources/text.txt";
+
+    public static String readStr() {
+        String str = null;
+        try (Stream<String> lineStream = Files.lines(Path.of(fileName))) {
+            str = lineStream
+                    .map(String::toLowerCase)
+                    .reduce("", (string1, string2) -> string1.concat(string2 + " "))
+                    .replaceAll("\\p{Punct}|«|»|—", "");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return str;
+    }
+}
